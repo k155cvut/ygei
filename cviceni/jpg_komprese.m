7455c5d2d3f3147f500094da9e28d2db09c09709
bayertom <bayertom@natur.cuni.cz>
2023-10-11 11:41:22 +0200

    cv2

--- cviceni/jpg_komprese.m
+++ cviceni/jpg_komprese.m
@@ -1,12 +1,12 @@
 %****************************
-%JPG komprese pro matici 8x8
+%JPEG COMPRESSION / DECOMPRESSION OF THE IMAGE
 %****************************
 
 clc
 clear
 
 %Load figure and show
-fig1 = imread('image1.bmp');
+fig1 = imread('image2.bmp');
 imshow(fig1,[0 80]);
 
 %RGB components
@@ -70,7 +70,7 @@ for i = 1 : 8 : m-7
         CRSD=round(CRSD);
         
         %Compression factor
-        q=100;
+        q=20;
         QY_Y=50*QY/q;
         QC_C=50*QC/q;
         
@@ -85,3 +85,66 @@ for i = 1 : 8 : m-7
         CRT(i:i+7, j:j+7) = FCRS;
     end       
 end
+
+%********************************
+% JPEG DECOMPRESSION
+%********************************
+
+%Process by submatrices one by one
+for i = 1 : 8 : m-7
+    for j = 1 : 8 : n-7
+
+        %Get submatrix
+        YS = YT(i:i+7, j:j+7);
+        CBS = CBT(i:i+7, j:j+7);
+        CRS = CRT(i:i+7, j:j+7);
+        
+        %Dequantization
+        YSD=round(YS.*QY_Y);
+        CBSD=round(CBS.*QC_C);
+        CRSD=round(CRS.*QC_C);
+
+        %Inverse DCT
+        YSI = myidct2(YSD );
+        CBSI = myidct2(CBSD);
+        CRSI = myidct2(CRSD);
+    
+        %Set compresed submatrix
+        YT(i:i+7, j:j+7) = YSI;
+        CBT(i:i+7, j:j+7) = CBSI;
+        CRT(i:i+7, j:j+7) = CRSI;
+    end       
+end
+
+%Conversion RGB to YCbCr
+RI =  1.0000 * YT - 0.0000 * (CBT-128) + 1.4020* (CRT-128);
+GI =  1.0000 * YT - 0.3441 * (CBT-128) - 0.7141 * (CRT-128);
+BI =  1.0000 * YT + 1.7720 * (CBT-128) - 0.0001 * (CRT-128);
+
+%Set RGB components
+fig2(:,:,1) = uint8(RI);
+fig2(:,:,2) = uint8(GI);
+fig2(:,:,3) = uint8(BI);
+
+%Plot image
+imtool(fig2);
+
+%Differencies in RGB components
+dR = RI-R;
+dG = GI-G;
+dB = BI-B;
+
+dR2 = dR.*dR;
+dG2 = dG.*dG;
+dB2 = dB.*dB;
+
+dR2Sum = sum(sum(dR2));
+dG2Sum = sum(sum(dG2));
+dB2Sum = sum(sum(dB2));
+
+% Errors in RGB components
+mR = sqrt(dR2Sum/ (m*n))
+mG = sqrt(dG2Sum/ (m*n))
+mB = sqrt(dB2Sum/ (m*n))
+
+
